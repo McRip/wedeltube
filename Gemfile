43af348af5edac984474002377d90c0d92f1bec3
Moritz Kröger <moritz.kroeger@googlemail.com>
2011-05-19 18:01:00 +0200

    rails init

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rack'
+gem 'rails', '3.0.7'
+gem 'mysql'
+gem 'capistrano'
+
+gem 'ruby-debug19', :require => 'ruby-debug'
+
+# group :development, :test do
+# end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedeltube::Application.config.secret_token = 'b5575c644c333f8f417e2372e89e9d9ed61adc9a24daafcd7e60b957c4a6f5eefbe6c5e4cf234871be8ceeb1a0a4def3bc25f2022d95cc8d55446737988a24a9'
