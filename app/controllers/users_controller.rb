978d8dbe2adc1d160feb785eecaa7d6a2f4fddf5
Moritz Kröger <moritz.kroeger@googlemail.com>
2011-08-28 14:06:18 +0200

    added User Avatar Upload tags are now only editable by video owner encoding fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,26 +10,6 @@ class UsersController < ApplicationController
     
   end
 
-  def new
-
-  end
-
-  def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def destroy
-
-  end
-
   private
 
   def find_user
