24278bfc3edd5655adcac51aaf63175151b76151
user <user@ubuntu.localdomain>
2011-08-28 15:13:43 +0200

    Favoritenseite erstellt

--- config/routes.rb
+++ config/routes.rb
@@ -15,24 +15,9 @@ Wedeltube::Application.routes.draw do
 
   resources :users do
     resources :comments, :only => [ :create, :update, :destroy ]
-    resources :favorites, :only => [ :create, :update, :destroy ]
+    resources :favorites, :only => [ :index, :create, :update, :destroy ]
     resources :videos
     resources :participants, :only => [ :create, :update, :destroy ]
-  end
-
-  resources :participants do
-    resources :videos
-    resource :user
-  end
-
-  resources :favorites do
-    resource :user
-    resource :video
-  end
-
-  resources :comments do
-    resource :user
-    resource :video
   end
 
   resources :tags, :only => [ :index, :show, :create, :update, :destroy ] do
