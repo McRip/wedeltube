9e945b50be9f8c5a33e1c000b45de71add415c16
user <user@ubuntu.localdomain>
2011-09-12 11:14:19 +0200

    Controller-Tests

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Tag do
 
   it "should create a valid Tag" do
-    Tag.new(:id => 0, :name => "Test").should be_valid
+    Tag.new(:id => 1, :name => "Test").should be_valid
    end
 end
