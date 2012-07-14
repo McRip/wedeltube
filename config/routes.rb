fcf26e023cf69d5e5b4e6c02cd15f1a39d3237de
user <user@ubuntu.localdomain>
2012-07-14 16:01:30 +0200

    Terms of use2

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,7 @@ Wedeltube::Application.routes.draw do
   end
   
   match "/imprint" => 'home#imprint', :as => 'imprint'
+  match "/terms_of_use" => 'home#terms_of_use', :as => 'terms_of_use'
 
   root :to => "home#index"
 end
